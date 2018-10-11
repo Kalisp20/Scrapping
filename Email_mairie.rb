eccfdb906a9c6bdaba200a6835c48d3fdf749c8d
Kalisp20 <hanane.duissi@yahoo.com>
2018-10-11 23:54:55 +0200

    modiffication

--- Email_mairie.rb
+++ Email_mairie.rb
@@ -3,30 +3,29 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_all_url
-
-	@nom_mairie=[]
-    @hypertext=[]
-    @email=[]
-
-	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))   
-	page.css("a.lientxt").each do |node|
-
-		@hypertext << [(node['href'].slice!(1,1000))]  
-		@nom_mairie << node.text
-    end
-	
-    @hypertext.each do |route|
-        puts "blabla#{route}"
-
-        def get_the_email
-
-	        page = Nokogiri::HTML(open("http://annuaire-des-mairies.com#{route}"))
-            @email<< page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
-            
-        end
     
+    @nom_mairie=[]
+    @email=[]
+    
+    page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    page.css("a.lientxt").each do |node|
+        
+        @hypertext = [(node['href'].slice!(1,1000))]
+        @nom_mairie << node.text
+        
+        def get_the_email
+        
+        page = Nokogiri::HTML(open("http://annuaire-des-mairies.com#{@hypertext}"))
+        @email<< page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
+        
     end
-
+    
+    get_the_email
+    @email.each do |email|
+        puts email
+        ends
+        
+    end
 end
 
 get_all_url
